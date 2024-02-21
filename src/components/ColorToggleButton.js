11cfe87d28bae733e972cdcd42af575d964524ad
ZiYi <30764272+lilakima@users.noreply.github.com>
2024-02-21 22:07:26 +0800

    refactor: Remove redundant file

--- src/components/ColorToggleButton.js
+++
@@ -1,7 +0,0 @@
-const ColorToggleButton = ({ mode, toggleMode, color, label }) => (
-    <button className={`bg-${color} text-white px-4 py-2 rounded`} onClick={() => toggleMode(color)}>
-      {label}
-    </button>
-);
-
-export default ColorToggleButton;
