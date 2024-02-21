987c337948a6c56207d82c26cf098adce4913f26
ZiYi <30764272+lilakima@users.noreply.github.com>
2024-02-21 21:53:30 +0800

    refactor: Extract dummyData

--- src/App.js
+++ src/App.js
@@ -6,28 +6,6 @@ import ColorToggleButton from './components/ColorToggleButton';
 import { sliderButtons, gridItems } from './dummyData';
 import './App.css';
 
-const gridItems = [
-  { id: 'A', label: 'A' },
-  { id: 'B', label: 'B' },
-  { id: 'C', label: 'C' },
-  { id: 'D', label: 'D' },
-  { id: 'E', label: 'E' },
-  { id: 'F', label: 'F' },
-  { id: 'G', label: 'G' },
-  { id: 'H', label: 'H' },
-  { id: 'I', label: 'I' },
-  { id: 'J', label: 'J' },
-]
-
-const sliderButtons = [
-  { id: 1, label: "MagnifyingGlassIcon", special: true },
-  { id: 2, label: "ALL", special: true },
-  { id: 3, label: "TYPE1", special: false },
-  { id: 4, label: "TYPE2", special: false },
-  { id: 5, label: "TYPE3", special: false },
-  { id: 6, label: "TYPE4", special: false },
-];
-
 const App = () => {
   const [mode, setMode] = useState('dark'); 
 
